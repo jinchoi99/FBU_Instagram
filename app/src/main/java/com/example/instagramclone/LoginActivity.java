0e6221890f741502923610aa66c4862f671fc12c
Jinny Choi <jinchoi99@fb.com>
2020-07-07 05:48:59 -0700

    Login with ParseUser.loginInBackground and go to MainActivity

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -2,11 +2,17 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG = "LoginActivity";
@@ -37,6 +43,25 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password){
         Log.i(TAG,"Attempting to login user" + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                //if request succeeded, exception e becomes null
+                if(e!=null){
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                //go to mainActivity if login succeed
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
+    private void goMainActivity() {
+        //Intent takes in two parameters: context,  what class you want to navigate to
+        //this is refering to this activity = LoginActivity, Activity is an instance of a Context
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }

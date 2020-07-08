07f5ea02117b327aa14b88fc9b5170adb53665fc
Jinny Choi <jinchoi99@fb.com>
2020-07-08 01:43:55 -0700

    Add Getters, Setters for Post Model

--- app/src/main/java/com/example/instagramclone/Post.java
+++ app/src/main/java/com/example/instagramclone/Post.java
@@ -1,8 +1,37 @@
 package com.example.instagramclone;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Post")
 public class Post extends ParseObject {
+    //define getters and setters based on keys (names of columns in parse dashboard) we've defined
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "user";
+
+    public String getDescription(){
+        //getString is a method defined in ParseObject class
+        //looks inside post parseobject and pulls out attribute w/ key name of 'description'
+        return getString(KEY_DESCRIPTION);
+    }
+    public void setDescription(String description){
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_IMAGE);
+    }
+    public void setImage(ParseFile image){
+        put(KEY_IMAGE, image);
+    }
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
 }
